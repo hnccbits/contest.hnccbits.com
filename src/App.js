24bb0e1fe374ffa6e7703526e0bcdb777e7a6860
Zeeshan Ashraf <ashraf2k1zeeshan@gmail.com>
2020-09-15 01:36:57 +0530

    Added LeaderBoard

--- src/App.js
+++ src/App.js
@@ -2,86 +2,87 @@ import React, { Component } from "react";
 import Nav from "./components/Navbar/Nav";
 import Header from "./components/Header";
 import "./App.css";
-import RankList from "./components/RankList";
-import Footer from "./components/Footer/Footer"
+// import RankList from "./components/RankList";
+import Footer from "./components/Footer/Footer";
+import LeaderBoard from "./components/LeaderBoard/LeaderBoard"
 
-const data = [
-  {
-    contest_name: "Contest 1",
-    contest_date: "12th August 2020",
-    contest_description: "Lorem Ipsum",    
-    winner: {
-      rank_one: {
-        name: "Name 1",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-      rank_two: {
-        name: "Name 2",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-      rank_three: {
-        name: "Name 3",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-    },
-  },
-  {
-    contest_name: "Contest 2",
-    contest_description: "Lorem Ipsum",
-    contest_date: "12th August 2020",
-    winner: {
-      rank_one: {
-        name: "Name 1",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-      rank_two: {
-        name: "Name 2",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-      rank_three: {
-        name: "Name 3",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-    },
-  },
-  {
-    contest_name: "Contest 3",
-    contest_date: "12th August 2020",
-    contest_description: "Lorem Ipsum",
-    winner: {
-      rank_one: {
-        name: "Name 1",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-      rank_two: {
-        name: "Name 2",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-      rank_three: {
-        name: "Name 3",
-        branch: "IT",
-        roll_no: 1811010,
-        codechef_id: "user_id",
-      },
-    },
-  },
-];
+// const data = [
+//   {
+//     contest_name: "Contest 1",
+//     contest_date: "12th August 2020",
+//     contest_description: "Lorem Ipsum",    
+//     winner: {
+//       rank_one: {
+//         name: "Name 1",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//       rank_two: {
+//         name: "Name 2",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//       rank_three: {
+//         name: "Name 3",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//     },
+//   },
+//   {
+//     contest_name: "Contest 2",
+//     contest_description: "Lorem Ipsum",
+//     contest_date: "12th August 2020",
+//     winner: {
+//       rank_one: {
+//         name: "Name 1",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//       rank_two: {
+//         name: "Name 2",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//       rank_three: {
+//         name: "Name 3",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//     },
+//   },
+//   {
+//     contest_name: "Contest 3",
+//     contest_date: "12th August 2020",
+//     contest_description: "Lorem Ipsum",
+//     winner: {
+//       rank_one: {
+//         name: "Name 1",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//       rank_two: {
+//         name: "Name 2",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//       rank_three: {
+//         name: "Name 3",
+//         branch: "IT",
+//         roll_no: 1811010,
+//         codechef_id: "user_id",
+//       },
+//     },
+//   },
+// ];
 
 class App extends Component {
   render() {
@@ -89,10 +90,12 @@ class App extends Component {
       <div className="App">
         <Nav />
         <Header />
-
+{/* 
         {data.map((data) => {
           return <RankList data={data} />;
-        })}
+        })} */}
+        <h2 className="lb-title">LEADERBOARD</h2>
+        <LeaderBoard />
         <Footer/>
       </div>
       
