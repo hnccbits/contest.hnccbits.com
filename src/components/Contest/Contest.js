7af15524257f6270aa410c7e6d6bfd4bee62181b
moonstoper <sjkachhap@outlook.com>
2020-10-05 20:26:07 +0530

    footer css/contest.js-minor-change

--- src/components/Contest/Contest.js
+++ src/components/Contest/Contest.js
@@ -5,7 +5,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import GetAppIcon from '@material-ui/icons/GetApp';
+//import GetAppIcon from '@material-ui/icons/GetApp';
 import Details from './Details';
 
 const useStyles = makeStyles((theme) => ({
