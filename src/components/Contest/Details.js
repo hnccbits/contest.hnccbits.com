823e59d68ed4a14a5d4c583fe168336c57a21499
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-11-04 10:56:40 +0530

    Updated: BIT Code November Round - 1

--- src/components/Contest/Details.js
+++ src/components/Contest/Details.js
@@ -1,120 +1,144 @@
-import Image from '../../assets/cc-logo.png';
+import Image from "../../assets/cc-logo.png";
 const Details = [
   {
-    contestName: 'BIT Code: September Round',
-    date: 'September 17, 2023',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: November Round - 1",
+    date: "November 02, 2024",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://www.hackerrank.com/bit-code-september',
-    contestCode: '#',
-    editorialCode: '2023-09-13-bitcode-August-editorial'
-  },{
-    contestName: 'BIT Code: August Round',
-    date: 'August 23, 2023',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
-    image: { Image },
-    registrationLink: 'https://www.hackerrank.com/bit-code-august-1',
-    contestCode: '#',
-    editorialCode: '2023-09-13-bitcode-August-editorial'
+    registrationLink: "https://forms.gle/YL4wN3sqAq6QaJUQA",
+    contestCode: "bit-code-phase-1-round-1",
+    editorialCode: "2024-11-02-bitcode-November-editorial",
   },
   {
-    contestName: 'BIT Code: July Round',
-    date: 'July 20, 2023',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: September Round",
+    date: "September 17, 2023",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://www.hackerrank.com/bit-code-july',
-    contestCode: '#',
-    editorialCode: '2023-07-29-BITCode-July-editorial'
+    registrationLink: "https://www.hackerrank.com/bit-code-september",
+    contestCode: "#",
+    editorialCode: "2023-09-13-bitcode-August-editorial",
   },
   {
-    contestName: 'BIT Code: June Round',
-    date: 'June 10, 2023',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: August Round",
+    date: "August 23, 2023",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://www.hackerrank.com/hncc-bit-code-june',
-    contestCode: '#',
-    editorialCode: '2023-06-15-BITCode-June-Editorial'
+    registrationLink: "https://www.hackerrank.com/bit-code-august-1",
+    contestCode: "#",
+    editorialCode: "2023-09-13-bitcode-August-editorial",
   },
   {
-    contestName: 'BIT Code: February Round',
-    date: 'February 25, 2023',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: July Round",
+    date: "July 20, 2023",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://www.hackerrank.com/bit-code-feb',
-    contestCode: '#',
-    editorialCode: '2023-06-03-HACK-O-VERSE'
+    registrationLink: "https://www.hackerrank.com/bit-code-july",
+    contestCode: "#",
+    editorialCode: "2023-07-29-BITCode-July-editorial",
   },
   {
-    contestName: 'BIT Code: October Round',
-    date: 'October 16, 2022',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: June Round",
+    date: "June 10, 2023",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://www.hackerrank.com/bit-code-october',
-    contestCode: '#',
-    editorialCode: '2022-10-27-BITCode-Oct-Editorial'
+    registrationLink: "https://www.hackerrank.com/hncc-bit-code-june",
+    contestCode: "#",
+    editorialCode: "2023-06-15-BITCode-June-Editorial",
   },
   {
-    contestName: 'BIT Code: July Round',
-    date: 'July 17, 2022',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: February Round",
+    date: "February 25, 2023",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://www.hackerrank.com/hncc-bit-code-july',
-    contestCode: '#',
-    editorialCode: '2022-06-30-BITCode-June-Editorial'
+    registrationLink: "https://www.hackerrank.com/bit-code-feb",
+    contestCode: "#",
+    editorialCode: "2023-06-03-HACK-O-VERSE",
   },
   {
-    contestName: 'BIT Code: June Round',
-    date: 'June 11, 2022',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: October Round",
+    date: "October 16, 2022",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://www.hackerrank.com/hncc-bitcode-june',
-    contestCode: '#',
-    editorialCode: '2022-06-30-BITCode-June-Editorial'
+    registrationLink: "https://www.hackerrank.com/bit-code-october",
+    contestCode: "#",
+    editorialCode: "2022-10-27-BITCode-Oct-Editorial",
   },
   {
-    contestName: 'BIT Code: March Round',
-    date: 'March 23, 2021',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: July Round",
+    date: "July 17, 2022",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://bit.ly/bitcdmar',
-    contestCode: 'BCDE2021',
-    editorialCode: '2021-03-23-BITCode-Mar-Editorial'
+    registrationLink: "https://www.hackerrank.com/hncc-bit-code-july",
+    contestCode: "#",
+    editorialCode: "2022-06-30-BITCode-June-Editorial",
   },
   {
-    contestName: 'BIT Code: October Round',
-    date: 'October 30, 2020',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: June Round",
+    date: "June 11, 2022",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://bit.ly/bitcdoct',
-    contestCode: 'BITE2020',
-    editorialCode: '2020-11-03-BITCode-Oct-Editorial'
+    registrationLink: "https://www.hackerrank.com/hncc-bitcode-june",
+    contestCode: "#",
+    editorialCode: "2022-06-30-BITCode-June-Editorial",
   },
   {
-    contestName: 'BIT Code: September Round',
-    date: 'October 1, 2020',
-    description: 'Yeah we know that it is October. Chef arrived late. Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: March Round",
+    date: "March 23, 2021",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://bit.ly/bitcodesep',
-    contestCode: 'BTCD2020',
-    editorialCode: '2020-10-01-BITCode-Sep-Editorial'
+    registrationLink: "https://bit.ly/bitcdmar",
+    contestCode: "BCDE2021",
+    editorialCode: "2021-03-23-BITCode-Mar-Editorial",
   },
   {
-    contestName: 'BIT Code: August Round',
-    date: 'August 30, 2020',
-    description: 'Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.',
+    contestName: "BIT Code: October Round",
+    date: "October 30, 2020",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://forms.gle/bJzJptcNS5HgQYV69',
-    contestCode: 'COBI2020',
-    editorialCode: '2020-08-30-BITCodeAug_Editorial'
+    registrationLink: "https://bit.ly/bitcdoct",
+    contestCode: "BITE2020",
+    editorialCode: "2020-11-03-BITCode-Oct-Editorial",
   },
   {
-    contestName: 'The Monsoon Code',
-    date: 'July 30, 2020',
-    description: 'In association with TAP Cell, BIT Sindri. Part of the BIT Code Contest Page Application Process',
+    contestName: "BIT Code: September Round",
+    date: "October 1, 2020",
+    description:
+      "Yeah we know that it is October. Chef arrived late. Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
     image: { Image },
-    registrationLink: 'https://forms.gle/XxggLGrzqrx1uc4E6',
-    contestCode: 'TMSC2020',
-    editorialCode: '2020-07-31-tmsc'
+    registrationLink: "https://bit.ly/bitcodesep",
+    contestCode: "BTCD2020",
+    editorialCode: "2020-10-01-BITCode-Sep-Editorial",
+  },
+  {
+    contestName: "BIT Code: August Round",
+    date: "August 30, 2020",
+    description:
+      "Part of the Monthly Contest Series: BIT Code. The questions were primarily focused on DSA. In association with TAP Cell, BIT Sindri.",
+    image: { Image },
+    registrationLink: "https://forms.gle/bJzJptcNS5HgQYV69",
+    contestCode: "COBI2020",
+    editorialCode: "2020-08-30-BITCodeAug_Editorial",
+  },
+  {
+    contestName: "The Monsoon Code",
+    date: "July 30, 2020",
+    description:
+      "In association with TAP Cell, BIT Sindri. Part of the BIT Code Contest Page Application Process",
+    image: { Image },
+    registrationLink: "https://forms.gle/XxggLGrzqrx1uc4E6",
+    contestCode: "TMSC2020",
+    editorialCode: "2020-07-31-tmsc",
   },
 ];
 
